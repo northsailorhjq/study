308ba68227ea6467bfc52d8dd022aa22312a7260
northsailor <hjqsmu@163.com>
2017-03-27 18:06:37 +0800

    modified Test.java print

--- Test.java
+++ Test.java
@@ -3,11 +3,6 @@ package com.jkxy.test;
 public class Test {
 	public static void main(String[] args) {
 	
-
+		System.out.println("Test.java");
 	}
-}
-class ArrayMaxNum{
-	int i,max;
-	private int A[]={47,48,30,17,62,45,89,56,13};
-	max
 }
