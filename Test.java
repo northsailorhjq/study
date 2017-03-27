1ebba928470328affe9792707ece54e33125f78c
northsailor <hjqsmu@163.com>
2017-03-27 17:49:51 +0800

    first time commit Test.java

---
+++ Test.java
@@ -0,0 +1,13 @@
+package com.jkxy.test;
+
+public class Test {
+	public static void main(String[] args) {
+	
+
+	}
+}
+class ArrayMaxNum{
+	int i,max;
+	private int A[]={47,48,30,17,62,45,89,56,13};
+	max
+}
